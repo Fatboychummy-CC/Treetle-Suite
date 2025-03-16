62416df764faca1ba2e74d7b9f3bcc63d564843b
Matthew W <fatboychummy@gmail.com>
2025-03-15 23:46:00 -0600

    Controltle: Basic integration with Treetle complete.

--- controltle.lua
+++ controltle.lua
@@ -11,15 +11,40 @@ local minilogger = require "minilogger"
 local _log = minilogger.new("Controltle")
 local catppuccin = require "catppuccin"
 local palette = catppuccin.set_palette("mocha")
+local smn = require "single_modem_network"
+local thread = require "thread"
 minilogger.set_log_level(... and minilogger.LOG_LEVELS[(...):upper()] or minilogger.LOG_LEVELS.INFO)
 term.setBackgroundColor(palette.crust)
 term.setTextColor(palette.text)
 term.clear()
 
-local fuckyouwindow = window.create(term.current(), 0, 0, 100, 1, false)
-minilogger.set_log_window(fuckyouwindow)
+--#region Setup
+do
+  local modem_found
+  for _, side in ipairs(redstone.getSides()) do
+    if peripheral.hasType(side, "peripheral_hub") then
+      if modem_found then
+        error("Too many wired modems connected. Only connect one.")
+      end
+      smn.set_modem(side)
+      modem_found = true
+    end
+  end
+  if not modem_found then
+    error("No wired modem connected. Connect one.")
+  end
+
+  local log_to_nothing_window = window.create(term.current(), 0, 0, 100, 1, false)
+  minilogger.set_log_window(log_to_nothing_window)
+end
+--#endregion Setup
 
 --#region Constants
+
+--- The communication channel for Treetle.
+---@type integer
+local TREETLE_CHANNEL = 35000
+smn.open(TREETLE_CHANNEL)
 
 --- The item IDs of saplings that the turtle will plant.
 ---@type id_lookup
@@ -71,7 +96,12 @@ local PLANK_IDS = {
 --- The item IDs of "good" fuels that will be sent to turtles for refueling.
 ---@type id_lookup
 local FUEL_IDS = {
-  ["minecraft:charcoal_block"] = true
+  ["minecraft:coal_block"] = true,
+  ["minecraft:charcoal"] = true,
+  ["minecraft:coal"] = true,
+  ["minecraft:lava_bucket"] = true,
+  ["minecraft:dried_kelp_block"] = true,
+  ["minecraft:blaze_rod"] = true
 }
 
 ---@alias fuel_lookup table<string, integer> Maps fuel to their burn time.
@@ -83,7 +113,6 @@ local SMELT_FUEL_IDS = {
   ["minecraft:charcoal"] = 1600,
   ["minecraft:lava_bucket"] = 20000,
   ["minecraft:coal_block"] = 16000,
-  ["minecraft:charcoal_block"] = 16000,
   ["minecraft:dried_kelp_block"] = 4000,
   ["minecraft:blaze_rod"] = 2400,
 }
@@ -136,6 +165,13 @@ end
 
 --#endregion Configuration
 
+--#region logs
+
+local ui_log = minilogger.new("ui")
+local main_log = minilogger.new("main")
+
+--#endregion logs
+
 --#region Helper Functions
 
 
@@ -165,9 +201,6 @@ local function pull_events(...)
   return table.unpack(ev)
 end
 
---#endregion Helper Functions
-
---#region Main Functions
 
 
 --- Renders the main background, with nothing on it.
@@ -297,10 +330,268 @@ end
 
 
 
+--- Tween a value from A to B, using square easing.
+---@param a number The starting value.
+---@param b number The ending value.
+---@param t number The time, from 0 to 1.
+---@return number value The value at time t.
+local function tween(a, b, t)
+  return a + (b - a) * t^2
+end
+
+
+
+--- Fade in the main UI.
+local function fade_in()
+  local old_palette = {}
+  local crust_r, crust_g, crust_b = term.getPaletteColor(palette.crust)
+  for i = 0, 15 do
+    old_palette[i] = {term.getPaletteColor(2^i)}
+    term.setPaletteColor(2^i, crust_r, crust_g, crust_b)
+  end
+
+  render_background(MAIN_WINDOW)
+  MAIN_WINDOW.setVisible(true)
+
+  -- Le epic fade-in
+  local start_time = os.clock()
+  local anim_duration = 1
+  while os.clock() - start_time < anim_duration do
+    local t = (os.clock() - start_time) / anim_duration
+    for i = 0, 15 do
+      term.setPaletteColor(2^i, tween(crust_r, old_palette[i][1], t), tween(crust_g, old_palette[i][2], t), tween(crust_b, old_palette[i][3], t))
+    end
+    sleep()
+  end
+end
+
+
+
+--- Sends `n` items of the given type to the given turtle, from the given inventory.
+---@param inv_name string The name of the inventory to send items from.
+---@param turtle_name string The name of the turtle to send items to.
+---@param item_types id_lookup The type of item to send.
+---@param n integer The number of items to send.
+---@return integer sent The number of items actually sent.
+local function send_items(inv_name, turtle_name, item_types, n)
+  expect(1, inv_name, "string")
+  expect(2, turtle_name, "string")
+  expect(3, item_types, "table")
+  expect(4, n, "number")
+
+  local sent = 0
+
+  -- Get info about what is in the inventory.
+  local items = smn.call(inv_name, "list")
+
+  -- Send the items to the turtle.
+  for slot, item in pairs(items) do
+    if item_types[item.name] then
+      sent = sent + smn.call(inv_name, "pushItems", turtle_name, slot, n - sent)
+
+      if sent >= n then
+        break
+      end
+    end
+  end
+
+  local names = {}
+  for name in pairs(item_types) do
+    table.insert(names, name)
+  end
+  main_log.debug("Sent", sent, "of", n, "items (", table.concat(names, ", "), ") to", turtle_name, "from", inv_name)
+
+  return sent
+end
+
+
+
+--- Sends `n` items of the given type to the given turtle, from storage.
+---@param turtle_name string The name of the turtle to send items to.
+---@param item_types id_lookup The type of item to send.
+---@param n integer The number of items to send.
+---@return integer sent The number of items actually sent.
+local function send_items_from_storage(turtle_name, item_types, n)
+  expect(1, turtle_name, "string")
+  expect(2, item_types, "table")
+  expect(3, n, "number")
+
+  local sent = 0
+
+  for _, storage in ipairs(config.storages) do
+    sent = sent + send_items(storage, turtle_name, item_types, n - sent)
+
+    if sent >= n then
+      break
+    end
+  end
+
+  return sent
+end
+
+
+
+--- Count 'important' items in the storage chests.
+---@return integer logs The number of logs in the storage chests.
+---@return integer planks The number of planks in the storage chests.
+---@return integer saplings The number of saplings in the storage chests.
+---@return integer fuels The number of 'good' fuels in the storage chests.
+local function count_important()
+  local funcs = {}
+
+  local logs, planks, saplings, fuels = 0, 0, 0, 0
+  for _, inventory_name in ipairs(config.storages) do
+    table.insert(funcs, function()
+      local items = smn.call(inventory_name, "list")
+      for _, item in pairs(items) do
+        if LOG_IDS[item.name] then
+          logs = logs + item.count
+        elseif PLANK_IDS[item.name] then
+          planks = planks + item.count
+        elseif SAPLING_IDS[item.name] then
+          saplings = saplings + item.count
+        elseif FUEL_IDS[item.name] then
+          fuels = fuels + item.count
+        end
+      end
+    end)
+  end
+
+  parallel.waitForAll(table.unpack(funcs)) -- Hopefully people don't use more than 256 storages.
+
+  return logs, planks, saplings, fuels
+end
+
+
+
+--- Sends all items in the given inventory to storage.
+---@param inv_name string The name of the inventory to send items from.
+---@return boolean success False if any item failed to move.
+local function send_all_to_storage(inv_name)
+  expect(1, inv_name, "string")
+
+  --- Attempt to move all items in the inventory into the given storage, once.
+  ---@param storage string The name of the storage to move items to.
+  ---@return boolean success False if any item failed to move.
+  local function attempt_once(storage)
+    local funcs = {}
+    local items = smn.call(inv_name, "list")
+
+    for slot, item in pairs(items) do
+      table.insert(funcs, function()
+        smn.call(inv_name, "pushItems", storage, slot, item.count)
+      end)
+    end
+
+    parallel.waitForAll(table.unpack(funcs))
+
+    return next(smn.call(inv_name, "list")) == nil
+  end
+
+  for _, storage in ipairs(config.storages) do
+    if attempt_once(storage) then
+      return true
+    end
+  end
+
+  return false
+end
+
+
+
+local intermediate_locked = false
+--- Takes items from the given turtle
+--- Leaves the following in the inventories:
+--- 1. Fuel, x16
+--- 2. Saplings, x16
+---@param turtle_name string The name of the turtle to take items from.
+local function take_back_items(turtle_name)
+  expect(1, turtle_name, "string")
+
+  main_log.debug("Handling", turtle_name)
+
+  if not config.intermediate_chest or not smn.isPresent(config.intermediate_chest) then
+    return 0
+  end
+
+  while intermediate_locked do
+    sleep()
+  end
+
+  intermediate_locked = true
+
+  -- Take everything from the turtle, moving it into the intermediate chest.
+  for slot = 1, 16 do
+    smn.call(config.intermediate_chest, "pullItems", turtle_name, slot)
+  end
+
+  -- Determine what is now in the intermediate chest.
+  local items = smn.call(config.intermediate_chest, "list")
+
+  -- Send 16 fuel and 16 saplings back to the turtle (if they exist).
+  -- a. Fuel
+  local sent_fuel = send_items(config.intermediate_chest, turtle_name, FUEL_IDS, 16)
+  -- b. Saplings
+  local sent_saplings = send_items(config.intermediate_chest, turtle_name, SAPLING_IDS, 16)
+
+  main_log.debug("Sent", sent_fuel, "fuel and", sent_saplings, "saplings back to", turtle_name)
+
+  -- And send everything else to the storages.
+  send_all_to_storage(config.intermediate_chest)
+
+  -- If we sent less than 16 fuel or saplings, check if there are more in the storages, and send them.
+  -- However:
+  -- 1. Only send fuel if there it leaves us with at least 4 fuel in the storages.
+  -- 2. If less than 32 saplings and we didn't send anything back, send a single sapling.
+  -- 3. Otherwise, send 16 of each.
+  local _, _, saplings, fuels = count_important()
+  main_log.debug("Storage Fuel:", fuels, "\nStorage Saplings:", saplings)
+
+  if fuels - (16 - sent_fuel) >= 4 then
+    main_log.debug("Sending", 16 - sent_fuel, "fuel from storage to", turtle_name)
+    send_items_from_storage(turtle_name, FUEL_IDS, 16 - sent_fuel)
+  end
+
+  if saplings < 32 and sent_saplings == 0 then
+    main_log.debug("Sending", 1, "sapling from storage to", turtle_name)
+    send_items_from_storage(turtle_name, SAPLING_IDS, 1)
+  else
+    main_log.debug("Sending", 16 - sent_saplings, "saplings from storage to", turtle_name)
+    send_items_from_storage(turtle_name, SAPLING_IDS, 16 - sent_saplings)
+  end
+
+  main_log.debug("Done handling", turtle_name)
+
+  intermediate_locked = false
+end
+
+
+
+--- Notifies a turtle that its okay to go.
+---@param turtle_id integer The name of the turtle to notify.
+local function notify_turtle(turtle_id)
+  expect(1, turtle_id, "number")
+
+  sleep(2) -- Ensure the turtle has had time to do whatever it needs to do.
+
+  smn.transmit(TREETLE_CHANNEL, TREETLE_CHANNEL, {
+    action = "treetle_go",
+    turtle_id = turtle_id
+  })
+
+  main_log.debug("Notified turtle", turtle_id, "that it can continue.")
+end
+
+
+
+--#endregion Helper Functions
+
+--#region Main Functions
+
+
+
 --- Displays the help screens for setup.
 local function display_help()
-  help_displaying = true
-
   local pages_needed = 10 -- Tweaked as I need more help pages.
   local long_window = window.create(term.current(), T_W + 1, 1, T_W * (pages_needed + 1), T_H)
   local pages = {}
@@ -385,15 +676,6 @@ local function display_help()
     end
   end
 
-  --- Tween a value from A to B, using square easing.
-  ---@param a number The starting value.
-  ---@param b number The ending value.
-  ---@param t number The time, from 0 to 1.
-  ---@return number value The value at time t.
-  local function tween(a, b, t)
-    return a + (b - a) * t^2
-  end
-
   -- Write the help pages.
   write_centered(pages[1], "Welcome to Controltle!", nil, -4)
   write_centered(pages[1], "This program is made to work with Treetle, to slowly automate fuel creation!", palette.subtext_0)
@@ -476,14 +758,14 @@ local function display_help()
 
   animate_page_in() -- Bring in the first page.
   while current_page <= pages_needed do
-    local ev, param = pull_events("mouse_click", "key_up")
+    local ev, param = pull_events("mouse_click", "key")
     if ev == "mouse_click" then
       if param == 1 then
         animate_page_in()
       elseif param == 2 then
         animate_page_out()
       end
-    elseif ev == "key_up" then
+    elseif ev == "key" then
       if param == keys.enter or param == keys.space or param == keys.right or param == keys.d then
         animate_page_in()
       elseif param == keys.left or param == keys.a then
@@ -493,42 +775,46 @@ local function display_help()
   end
 
   animate_page_in()
+end
 
-  local old_palette = {}
-  local crust_r, crust_g, crust_b = term.getPaletteColor(palette.crust)
-  for i = 0, 15 do
-    old_palette[i] = {term.getPaletteColor(2^i)}
-    term.setPaletteColor(2^i, crust_r, crust_g, crust_b)
-  end
 
-  render_background(MAIN_WINDOW)
-  MAIN_WINDOW.setVisible(true)
 
-  -- Le epic fade-in
-  local start_time = os.clock()
-  anim_duration = 1
-  while os.clock() - start_time < anim_duration do
-    local t = (os.clock() - start_time) / anim_duration
-    for i = 0, 15 do
-      term.setPaletteColor(2^i, tween(crust_r, old_palette[i][1], t), tween(crust_g, old_palette[i][2], t), tween(crust_b, old_palette[i][3], t))
+--- Run the main system.
+_log.debug("Main thread is", thread.new(function()
+  main_log.info("Starting Controltle.")
+  load_config()
+  main_log.debug("Entering main loop.")
+  while true do
+    local _, _, _, _, message = os.pullEvent("modem_message")
+    main_log.debug("Received message: ", textutils.serialize(message, {compact=true}))
+
+    if type(message) == "table" and message.action == "treetle_discovery" then
+      main_log.debugf("Received treetle discovery from %s, with ID %d.", message.turtle_name, message.turtle_id)
+      thread.new(take_back_items, message.turtle_name)
+        :after(notify_turtle, message.turtle_id)
+        :on_error(main_log.error, "\n\nThe above error occurred when dealing with treetle", message.turtle_id, "as", message.turtle_name)
     end
-    sleep()
   end
-end
+end).id)
 
 
 
---- Takes items from the given turtle
---- Leaves the following in the inventories:
---- 1. Fuel, x16
---- 2. Saplings, x16
----@param turtle_name string The name of the turtle to take items from.
----@return integer count The number of *logs* taken back.
-local function take_back_items(turtle_name)
-  expect(1, turtle_name, "string")
+--- UI Thread
+_log.debug("UI thread is", thread.new(function()
+  term.setBackgroundColor(palette.crust)
+  term.setTextColor(palette.text)
+  term.clear()
+  sleep(0.25)
+  if not next(config.furnaces) or not next(config.storages) or not config.intermediate_chest then
+    main_log.info("Displaying help.")
+    display_help()
+  end
 
-  return 0 -- TODO: Implement this.
-end
+  main_log.debug("Fade in.")
+  fade_in()
+
+  -- ...
+end).id)
 
 
 
@@ -536,19 +822,16 @@ end
 
 --#region Main Program
 
-local ok, err = pcall(function()
-  load_config()
+local ok, err = pcall(thread.run)
 
-  if not next(config.furnaces) or not next(config.storages) or not config.intermediate_chest then
-    display_help()
-  end
-  render_background(MAIN_WINDOW)
-end)
-
+pcall(catppuccin.reset_palette)
+term.setBackgroundColor(colors.black)
+term.setTextColor(colors.white)
+term.clear()
+term.setCursorPos(1, 1)
 if not ok then
-  pcall(catppuccin.reset_palette)
-  _log.error(err)
-  error(err)
+  _log.fatal(err)
+  error(err, 0)
 end
 
 --#endregion Main Program
